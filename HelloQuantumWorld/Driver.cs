ffe883bfda82ab5982c524192f433f911c99b606
Wickramaranga Abeygunawardhana <umstek@live.com>
2017-12-24 23:20:26 +0530

    Add quantum entanglement code

--- HelloQuantumWorld/Driver.cs
+++ HelloQuantumWorld/Driver.cs
@@ -10,15 +10,18 @@ namespace Quantum.HelloQuantumWorld
             using (var sim = new QuantumSimulator())
             {
                 // Try initial values
-                Result[] initials = { Result.Zero, Result.One };
-                foreach (var initial in initials)
+                Result[] initials = new Result[] { Result.Zero, Result.One };
+                foreach (Result initial in initials)
                 {
                     var res = BellTest.Run(sim, 1000, initial).Result;
-                    var (numZeros, numOnes) = res;
+                    var (numZeros, numOnes, agree) = res;
                     System.Console.WriteLine(
-                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
+                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agree,-4}");
                 }
             }
+
+            System.Console.WriteLine("Press any key to continue...");
+            System.Console.ReadKey();
 
             System.Console.WriteLine("Press any key to continue...");
             System.Console.ReadKey();
